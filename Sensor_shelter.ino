e7605fbe7f195bce579a88fa6334a44d07554db8
Diego Giovanazzi <diegogiovanazzi@gmail.com>
2017-05-26 14:42:45 -0300

    Funcional
    separador ';'
    envia ID;T;H

--- Sensor_shelter.ino
+++ Sensor_shelter.ino
@@ -1,53 +1,46 @@
-#include <TimerOne.h>
-
-
 #include "DHT.h"
+
 #define DHTPIN 8
 #define DHTTYPE DHT11
-
 
 DHT dht(DHTPIN, DHTTYPE);
 int sensorPin = A0;
 int ena485=2;
-int ID=1;
-String tag="Balcarce 24 y 25";
+int id=1;
+
 String inputString = "";         // a string to hold incoming data
 boolean stringComplete = false;  // whether the string is complete
 boolean loopSensor=false;
 float h =0;
 float t = 0;
-int contador=0;
-
 void setup() {
   pinMode(ena485, OUTPUT);
   digitalWrite(ena485,HIGH);Serial.begin(9600);
   dht.begin();// Iniciamos el sensor​
-  Serial.println(tag);
-  delay(50);
+  delay(30);
   digitalWrite(ena485,LOW);
-  Timer1.initialize(100000);  // 250 ms
-  Timer1.attachInterrupt( ISR_CheckSerial) ;
+ 
 }
 
 void loop() {
  
 //   serialEvent();
    while(loopSensor)
-   { contador++;
-     h=dht.readHumidity();
+   {    h=dht.readHumidity();
         t=dht.readTemperature();
         digitalWrite(ena485,HIGH);
-        delay(50);
+        delay(30);
         if (isnan(t) || isnan(h)) {
-            Serial.println("ID 1 00.00 00.00");
+            Serial.print(id);
+            Serial.println(";00.00;00.00");
+            
         } else {
-            Serial.print("N:");Serial.print(contador);
-            Serial.print(" Temp:");Serial.print(t);
-            Serial.print(" Hum:");Serial.print(h);
-            Serial.print(" - ");Serial.println(tag);
+            Serial.print(id);Serial.print(";");
+            Serial.print(t);Serial.print(";");
+            Serial.println(h);
          
              };
-        delay(50);
+        delay(30);
   digitalWrite(ena485,LOW);
     //  serialEvent();
        if (stringComplete) 
@@ -62,9 +55,11 @@ void loop() {
    
    if (stringComplete) 
         {
-        comandos(inputString);
+       comandos(inputString);
+     
         inputString = "";
         stringComplete = false;
+      
         }
 }
 
@@ -72,14 +67,9 @@ void loop() {
 void serialEvent() {
  
   while (Serial.available()) {
-    // get the new byte:
     char inChar = (char)Serial.read();
-    // add it to the inputString:
-   
     inputString += inChar;
-    // if the incoming character is a newline, set a flag
-    // so the main loop can do something about it:
-    if (inChar == '\r') {
+     if (inChar == '\r') {
       stringComplete = true;
     }
   }
@@ -91,59 +81,30 @@ void comandos(String com){
   int sensorValue =0;
   float voltage=0;
   float temp=0;
-
-  char com_char;
-
-  com_char=inputString[0];
-
-  switch (com_char) {
-      case 't':
-        h=dht.readHumidity();
+  
+  if(id==inputString.toInt()){
+      h=dht.readHumidity();
         t=dht.readTemperature();
         digitalWrite(ena485,HIGH);
-        delay(50);
+        delay(5);
         if (isnan(t) || isnan(h)) {
-        Serial.println("ID 1 00.00 C 00.00 H");} 
-        else {
-            Serial.print("N:");Serial.print(contador);
-            Serial.print(" Temp:");Serial.print(t);
-            Serial.print(" Hum:");Serial.print(h);
-            Serial.print(" - ");Serial.println(tag);
-               };
-        break;
-      case 'b':
-        digitalWrite(ena485,HIGH);
-        delay(50);
-        sensorValue= analogRead(0);
-        Serial.print(sensorValue);Serial.println("ID ");  Serial.print(ID);
-        voltage= sensorValue*(5.0 / 1023.0);
-        Serial.println(voltage);
-        break;
-        
-   /*   case 'r':
-        digitalWrite(ena485,HIGH);
-        delay(50);
-        Serial.println("Encender Extractor");
-        break;*/
-      case 'l':
-        loopSensor=!loopSensor;
-        digitalWrite(ena485,HIGH);
-        delay(50);
-        Serial.print("loop:");
-        Serial.println(loopSensor);
-        break;  
-      default:
-        break;
-   }  
-   
-  delay(100);
+            Serial.print(id);
+            Serial.println(";00.00;00.00");
+            //Serial.print(char(10));
+          //  Serial.print(char(13));
+        } else {
+            Serial.print(id);Serial.print(";");
+            Serial.print(t);Serial.print(";");
+            Serial.println(h);
+            //Serial.print(char(10));Serial.print(char(13));
+            };
+        }
+     
+  delay(30);
   digitalWrite(ena485,LOW);
-  
-  
   }
   
   void ISR_CheckSerial(){
     serialEvent();
-  
   }
 
