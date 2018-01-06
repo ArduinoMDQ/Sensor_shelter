92951bf27eccbc5f2c68ecde6b7072ef282c7829
Diego Giovanazzi <diegogiovanazzi@gmail.com>
2018-01-06 15:09:38 -0300

    funcional, simulando 2 lugars con baliza

--- Sensor_shelter.ino
+++ Sensor_shelter.ino
@@ -13,7 +13,10 @@ boolean stringComplete = false;  // whether the string is complete
 boolean loopSensor=false;
 float h =0;
 float t = 0;
+int baliza = 0;
+
 void setup() {
+  
   pinMode(ena485, OUTPUT);
   digitalWrite(ena485,HIGH);Serial.begin(9600);
   dht.begin();// Iniciamos el sensor​
@@ -24,79 +27,65 @@ void setup() {
 
 void loop() {
  
-//   serialEvent();
-   while(loopSensor)
-   {    h=dht.readHumidity();
-        t=dht.readTemperature();
-        digitalWrite(ena485,HIGH);
-        delay(30);
-        if (isnan(t) || isnan(h)) {
-            Serial.print(id);
-            Serial.println(";00.00;00.00");
-            
-        } else {
-            Serial.print(id);Serial.print(";");
-            Serial.print(t);Serial.print(";");
-            Serial.println(h);
-         
-             };
-        delay(30);
-  digitalWrite(ena485,LOW);
-    //  serialEvent();
-       if (stringComplete) 
-        {
-        comandos(inputString);
-        inputString = "";
-        stringComplete = false;
-        }
-        delay(5000);
-   }
-   
-   
-   if (stringComplete) 
-        {
-       comandos(inputString);
-     
-        inputString = "";
-        stringComplete = false;
-      
-        }
+    if (stringComplete){
+       medicion();
+       inputString = "";
+       stringComplete = false;
+      }
+
 }
-
-
+ 
 void serialEvent() {
  
   while (Serial.available()) {
+    
     char inChar = (char)Serial.read();
     inputString += inChar;
-     if (inChar == '\r') {
+    if (inChar == '\r') {
       stringComplete = true;
     }
   }
- 
+
 }
 
-void comandos(String com){
-
+void medicion(){
+  int id2=id+1;
   int sensorValue =0;
   float voltage=0;
   float temp=0;
   
   if(id==inputString.toInt()){
-      h=dht.readHumidity();
+        h=dht.readHumidity();
         t=dht.readTemperature();
+        baliza=analogRead(sensorPin);
         digitalWrite(ena485,HIGH);
         delay(5);
         if (isnan(t) || isnan(h)) {
             Serial.print(id);
-            Serial.println(";00.00;00.00");
-            //Serial.print(char(10));
-          //  Serial.print(char(13));
+            Serial.println(";00.00;00.00;0");
         } else {
             Serial.print(id);Serial.print(";");
             Serial.print(t);Serial.print(";");
-            Serial.println(h);
-            //Serial.print(char(10));Serial.print(char(13));
+            Serial.print(h);Serial.print(";");
+            Serial.println(baliza);
+            };
+        }
+
+   
+  if((id2)==inputString.toInt()){
+        h=dht.readHumidity();
+        t=dht.readTemperature();
+        baliza=analogRead(sensorPin);
+        digitalWrite(ena485,HIGH);
+        delay(5);
+        if (isnan(t) || isnan(h)) {
+            Serial.print(id2);
+            Serial.println(";00.00;00.00;0");
+        } else {
+            Serial.print(id2);Serial.print(";");
+            Serial.print(t+1);Serial.print(";");
+            Serial.print(h+1);Serial.print(";");
+            Serial.println(baliza+176);
             };
         }
      
@@ -104,7 +93,7 @@ void comandos(String com){
   digitalWrite(ena485,LOW);
   }
   
-  void ISR_CheckSerial(){
+void ISR_CheckSerial(){
     serialEvent();
   }
 
